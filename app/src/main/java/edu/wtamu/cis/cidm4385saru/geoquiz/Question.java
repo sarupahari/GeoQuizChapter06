766cadde74c152733fb8134270090cba61553a2d
sarupahari <sarupahari@gmail.com>
2018-01-25 20:55:50 -0600

    Added Listener to the Text View  and Added a Previous Button

---
+++ app/src/main/java/edu/wtamu/cis/cidm4385saru/geoquiz/Question.java
@@ -0,0 +1,31 @@
+package edu.wtamu.cis.cidm4385saru.geoquiz;
+
+/**
+ * Created by saru on 1/25/2018.
+ */
+
+public class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+
+    public Question(int textResId, boolean answerTrue){
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+}
